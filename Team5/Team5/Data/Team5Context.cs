64835e9a577450695b468bd7870901e70739daf9
Aidan Justice <Aidan.Justice@mines.sdsmt.edu>
2022-02-18 15:41:08 -0700

    Ref #13 Created the student model

---
+++ Team5/Team5/Data/Team5Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Team5.Models;
+
+namespace Team5.Data
+{
+    public class Team5Context : DbContext
+    {
+        public Team5Context (DbContextOptions<Team5Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Team5.Models.Student> Student { get; set; }
+    }
+}
