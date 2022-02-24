44c4fc50baa8da2ba680ed3a85921f278da0f24f
Dakota Walker <Dakota.Walker@mines.sdsmt.edu>
2022-02-23 20:33:22 -0700

    Ref #4  Reverted to a previous commit, merged with branch that included student model, and started over. Registration now works with current project.

--- Team5/Team5/Data/Team5Context.cs
+++ Team5/Team5/Data/Team5Context.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Team5.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Team5.Data
 {
-    public class Team5Context : DbContext
+    public class Team5Context : IdentityDbContext
     {
         public Team5Context (DbContextOptions<Team5Context> options)
             : base(options)
--- Team5/Team5/Migrations/20220224030848_CreateIdentitySchema.Designer.cs
+++ Team5/Team5/Migrations/20220224030848_CreateIdentitySchema.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Team5.Data;
 
 namespace Team5.Migrations
 {
     [DbContext(typeof(Team5Context))]
-    partial class Team5ContextModelSnapshot : ModelSnapshot
+    [Migration("20220224030848_CreateIdentitySchema")]
+    partial class CreateIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
