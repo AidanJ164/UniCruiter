c6afb1d80f9b9c0420e56555d917226566dfedc3
Dakota Walker <Dakota.Walker@mines.sdsmt.edu>
2022-03-10 07:51:53 -0700

    Ref #28 Removed commented out code within startup.cs as referenced in the instructor review thread.

--- Team5/Team5/Startup.cs
+++ Team5/Team5/Startup.cs
@@ -41,7 +41,6 @@ namespace UniCruiter
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseDatabaseErrorPage();
             }
             else
             {
