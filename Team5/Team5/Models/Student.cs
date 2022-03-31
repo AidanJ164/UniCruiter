59cf1f7ab4aa7248db8bfff0c34329d4ee905a7c
Justice, Aidan M <aidan.justice@mines.sdsmt.edu>
2022-03-31 16:26:55 -0600

    Resolve "Requirement: Implement ViewModel Pattern (passed between Controllers and Views)"

--- Team5/Team5/Models/Student.cs
+++ Team5/Team5/Models/Student.cs
@@ -6,27 +6,10 @@ namespace UniCruiter.Models
     public class Student
     {
         public int Id { get; set; }
-
-        [Display(Name = "First Name")]
-        [StringLength(50, MinimumLength = 2)]
-        [Required]
         public string FirstName { get; set; }  
-
-        [Display(Name = "Last Name")]
-        [StringLength(50, MinimumLength = 2)]
-        [Required]
         public string LastName { get; set; }
-
-        [StringLength(50, MinimumLength = 2)]
-        [Required]
         public string Major { get; set; }
-
-        [StringLength(6, MinimumLength = 4)]
-        [Required]
         public string Season { get; set; }
-
-        [Range(2022, 3000)]
-        [Required]
         public int Year { get; set; }
         public string Email { get; set; }
     }
--- Team5/Team5/ViewModels/StudentViewModel.cs
+++ Team5/Team5/ViewModels/StudentViewModel.cs
@@ -1,18 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using UniCruiter.Models;
 
 namespace UniCruiter.ViewModels
 {
-    public class StudentSearchViewModel
+    public class StudentViewModel
     {
-        public List<Student> Students;
+        public StudentViewModel()
+        {
+
+        }
+
+        public StudentViewModel( Student student )
+        {
+            if(student != null)
+            {
+                Id = student.Id;
+                FirstName = student.FirstName;
+                LastName = student.LastName;
+                Major = student.Major;
+                Season = student.Season;
+                Year = student.Year;
+                Email = student.Email;
+            }
+        }
+
+        public int Id { get; set; }
+        
+        [Display(Name = "First Name")]
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
+        public string FirstName { get; set; }  
+
+        [Display(Name = "Last Name")]
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
+        public string LastName { get; set; }
+
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
+        public string Major { get; set; }
+
+        [StringLength(6, MinimumLength = 4)]
+        [Required]
+        public string Season { get; set; }
+
+        [Range(2022, 3000)]
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        public IEnumerable<Student> Students { get; set; }
         public SelectList Majors;
         public SelectList Years;
         public SelectList Seasons;
-        public string Season;
-        public int Year;
-        public string Major;
         public string SearchFirst;
         public string SearchLast;
     }
