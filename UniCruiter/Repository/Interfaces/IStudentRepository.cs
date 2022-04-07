f98bce83411dc98ed3e7a864dc675d78f6450668
Walker, Dakota - SDSMT Student <dakota.walker@mines.sdsmt.edu>
2022-04-07 12:10:02 -0600

    Resolve "Iteration 3 Instructor Review"

---
+++ UniCruiter/Repository/Interfaces/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UniCruiter.Models;
+using UniCruiter.ViewModels;
+
+namespace UniCruiter.Repository
+{
+    public interface IStudentRepository : IDisposable
+    {
+        Task<IList<Student>> GetStudents(StudentViewModel studentViewModel);
+        Task<Student> GetStudentByID(int studentID);
+        Task<Student> InsertStudent(StudentViewModel studentViewModel);
+        Task DeleteStudent(int studentID);
+        Task<Student> UpdateStudent(StudentViewModel studentViewModel);
+    }
+}
---
+++ UniCruiter/Repository/StudentRepository.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniCruiter.Data;
+using UniCruiter.Models;
+using UniCruiter.ViewModels;
+
+namespace UniCruiter.Repository
+{
+    public class StudentRepository : IStudentRepository, IDisposable
+    {
+        private UniCruiterContext _context;
+
+        public StudentRepository(UniCruiterContext context)
+        {
+            _context = context;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            //Cleanup created context
+            _context = null;
+        }
+
+
+        public async Task<Student> GetStudentByID(int studentId)
+        {
+            return await _context.Student.FirstOrDefaultAsync(s => s.Id == studentId);
+        }
+
+        public async Task<IList<Student>> GetStudents(StudentViewModel studentViewModel)
+        {
+            IQueryable<string> majorQuery = from m in _context.Student orderby m.Major select m.Major;
+            IQueryable<int> yearQuery = from y in _context.Student orderby y.Year select y.Year;
+            IQueryable<string> seasonQuery = from s in _context.Student orderby s.Season select s.Season;
+            
+            var students = from s in _context.Student
+                           select s;
+
+            //Filter by Name
+            if (!string.IsNullOrEmpty(studentViewModel.FirstName))
+            {
+                students = students.Where(s =>
+                    s.FirstName.StartsWith(studentViewModel.FirstName)
+                );
+            }
+            if (!string.IsNullOrEmpty(studentViewModel.LastName))
+            {
+                students = students.Where(s =>
+                    s.LastName.StartsWith(studentViewModel.LastName)
+                );
+            }
+
+            //Filter by Major
+            if (!string.IsNullOrEmpty(studentViewModel.Major))
+            {
+                students = students.Where(s => s.Major == studentViewModel.Major);
+            }
+
+            //Filter by Grad Season
+            if (!string.IsNullOrEmpty(studentViewModel.Season))
+            {
+                students = students.Where(s => s.Season == studentViewModel.Season);
+            }
+            
+            //Filter by Grad Year
+            if (studentViewModel.Year != 0)
+            {
+                students = students.Where(s => s.Year == studentViewModel.Year);
+            }
+
+            studentViewModel.Majors = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(majorQuery.Distinct());
+            studentViewModel.Seasons = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(seasonQuery.Distinct());
+            studentViewModel.Years = new Microsoft.AspNetCore.Mvc.Rendering.SelectList(yearQuery.Distinct());
+
+            return await students.ToListAsync();
+        }
+
+        public async Task<Student> InsertStudent(StudentViewModel studentViewModel)
+        {
+            Student student = new()
+            {
+                FirstName = studentViewModel.FirstName.Trim(),
+                LastName = studentViewModel.LastName.Trim(),
+                Major = studentViewModel.Major,
+                Season = studentViewModel.Season,
+                Year = studentViewModel.Year,
+                Email = studentViewModel.Email,
+        };
+
+            _context.Add(student);
+            await _context.SaveChangesAsync();
+
+            return student;
+
+        }
+
+        public async Task<Student> UpdateStudent(StudentViewModel studentViewModel)
+        {
+            Student student;
+
+            try
+            {
+                student = await _context.Student.FindAsync(studentViewModel.Id);
+
+                student.FirstName = studentViewModel.FirstName.Trim();
+                student.LastName = studentViewModel.LastName.Trim();
+                student.Major = studentViewModel.Major;
+                student.Season = studentViewModel.Season;
+                student.Year = studentViewModel.Year;
+                student.Email = studentViewModel.Email;
+
+                _context.Update(student);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(studentViewModel.Id))
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return student;
+
+        }
+
+        public async Task DeleteStudent(int studentID)
+        {
+
+            var student = _context.Student.FirstOrDefault(s => s.Id == studentID);
+
+            _context.Student.Remove(student);
+
+            await _context.SaveChangesAsync();
+
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Any(s => s.Id == id);
+        }
+    }
+}
