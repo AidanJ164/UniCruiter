014fa268e4dae4c2fc3e5075c5027c528d48f669
Justice, Aidan M <aidan.justice@mines.sdsmt.edu>
2022-04-11 11:59:25 -0600

    Resolve "Add "ApplicationUser" Model"

--- UniCruiter/Controllers/HomeController.cs
+++ UniCruiter/Controllers/HomeController.cs
@@ -1,22 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using UniCruiter.Models;
+using UniCruiter.Models.Identity;
 
 namespace UniCruiter.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
-            return View();
+            return View(await _userManager.GetUserAsync(User));
         }
 
         public IActionResult Privacy()
